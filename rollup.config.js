bb17d31bb8518b083ff2ab5c81cbb5f459068774
Jose Martin Llaneza <martinllaneza@gmail.com>
2018-07-09 21:34:10 +0800

    automatically define custom element based on tag arg or class name

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,14 @@ import typescript from 'rollup-plugin-typescript2';
 import {terser} from 'rollup-plugin-terser';
 
 export default {
-  input: 'src/counter-element.ts',
+  input: 'src/index.ts',
   output: {
     file: 'dist/bundle.js',
-    format: 'cjs'
+    format: 'umd',
+    name: 'counter-element'
   },
   plugins: [
     typescript({ cacheRoot: '.tmp'}),
-    terser()
+    terser({ keep_fnames: true })
   ]
 };
