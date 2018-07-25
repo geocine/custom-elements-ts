123d3aa2dd9c42d8c88c417eed4e32945e78c195
Aivan Monceller <aivan@monceller.com>
2018-07-25 21:37:27 +0800

    test(dispatch): additional dispatch and listen tests

--- tests/decorators.spec.ts
+++ tests/decorators.spec.ts
@@ -113,20 +113,26 @@ describe('decorators basic', () => {
 
 @CustomElement({
   tag: 'btn-listen-dispatch',
-  template: `<button></button>`
+  template: `<button>Test</button>`
 })
 export class ButtonElement extends HTMLElement {
 
   @Dispatch() btnClick: DispatchEmitter;
+  @Dispatch('btn.namedClick') btnClickNamed: DispatchEmitter;
 
   constructor() {
     super();
   }
-  
+
   @Listen('click')
   btnHandler(){
     this.shadowRoot.querySelector('button').innerHTML = 'Hello';
+    this.btnClick.emit({detail: 'Hello'});
+    this.btnClickNamed.emit({detail: 'Hello from named click'});
   }
+
+  @Listen('click', 'button')
+  btnInnerClick() {}
 
 }
 
@@ -134,13 +140,48 @@ describe('Event Listen.', () => {
   let element: any;
 
   beforeEach(() => {
-    const BtnElement = document.createElement('btn-listen-dispatch');
-    element = document.body.appendChild(BtnElement);
+    const btnElement = document.createElement('btn-listen-dispatch');
+    element = document.body.appendChild(btnElement);
   });
 
   it('should call method decorated @Listen', () => {
+    const btnHandlerSpsy = spyOn(element.btnHandler,'call');
+    element.click();
+    expect(btnHandlerSpsy).toHaveBeenCalled();
+  });
+
+  it('should trigger a btn.click DispatchEmitter', (done) => {
+    element.addEventListener('btn.click', (e) => {
+      expect(e.detail).toBe('Hello');
+      done();
+    });
+    element.click();
+  });
+
+  it('should trigger a btn.namedClick DispatchEmitter', (done) => {
+    element.addEventListener('btn.namedClick', (e) => {
+      expect(e.detail).toBe('Hello from named click');
+      done();
+    });
+    element.click();
+  });
+
+
+  it('should call method decorated @Listen on children element click', () => {
+    const btnHandlerSpsy = spyOn(element.btnHandler,'call');
+    element.shadowRoot.querySelector('button').click();
+    expect(btnHandlerSpsy).toHaveBeenCalled();
+  });
+
+  it('should call method decorated @Listen on selector click', () => {
+    const btnInnerSpsy = spyOn(element.btnInnerClick,'call');
+    element.shadowRoot.querySelector('button').click();
+    expect(btnInnerSpsy).toHaveBeenCalled();
+  });
+
+  it('should execute inner method decorated @Listen', () => {
     element.click();
     expect(element.shadowRoot.querySelector('button').innerHTML).toEqual('Hello');
-  })
+  });
 
 });
